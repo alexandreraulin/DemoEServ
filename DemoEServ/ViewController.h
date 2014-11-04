22e6cecb64f6856ea2d87f9378eee9e369690b9a
Alexandre Raulin <araulin@nextoo.fr>
2014-11-04 17:26:07 +0100

    Initial Commit

---
+++ DemoEServ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoEServ
+//
+//  Created by Alexandre Raulin on 04/11/2014.
+//  Copyright (c) 2014 Nextoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoEServ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DemoEServ
+//
+//  Created by Alexandre Raulin on 04/11/2014.
+//  Copyright (c) 2014 Nextoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
